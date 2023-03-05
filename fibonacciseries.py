be6d3812728e123278d05b4148b38ab459ec90d4
ankitgautam474 <127035981+ankitgautam474@users.noreply.github.com>
2023-03-05 22:10:58 +0530

    Add files via upload

---
+++ fibonacciseries.py
@@ -0,0 +1,20 @@
+
+#Note : The Fibonacci Sequence is the series of numbers :
+
+#0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+
+#Every next number is found by adding up the two numbers before it.
+
+
+n =50
+x = 0
+y = 1
+z = 0
+while z<=n:
+    print(z)
+    x=y
+    y=z
+    z=x+y
+
+
+
